bbdf84cace809791b590d27a9b8f5ab0897bf4a9
rohanpatel4846 <97083899+rohanpatel4846@users.noreply.github.com>
2023-02-25 17:51:56 +0530

    asd

--- KatapaDiscordBot/KatapaDiscordBot/Main/Bot.cs
+++ KatapaDiscordBot/KatapaDiscordBot/Main/Bot.cs
@@ -30,7 +30,7 @@ namespace KatapaDiscordBot.Main
                     await message.Channel.SendMessageAsync("Pong!");
                 }
             };
-
+            _client.Ready += Client_Ready;
             var token = ConfigJson.Token;
 
             await _client.LoginAsync(TokenType.Bot, token);
@@ -43,6 +43,45 @@ namespace KatapaDiscordBot.Main
         {
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
+        }
+
+        public async Task Client_Ready()
+        {
+            // Let's build a guild command! We're going to need a guild so lets just put that in a variable.
+            var guild = client.GetGuild(guildId);
+
+            // Next, lets create our slash command builder. This is like the embed builder but for slash commands.
+            var guildCommand = new SlashCommandBuilder();
+
+            // Note: Names have to be all lowercase and match the regular expression ^[\w-]{3,32}$
+            guildCommand.WithName("first-command");
+
+            // Descriptions can have a max length of 100.
+            guildCommand.WithDescription("This is my first guild slash command!");
+
+            // Let's do our global command
+            var globalCommand = new SlashCommandBuilder();
+            globalCommand.WithName("first-global-command");
+            globalCommand.WithDescription("This is my first global slash command");
+
+            try
+            {
+                // Now that we have our builder, we can call the CreateApplicationCommandAsync method to make our slash command.
+                await guild.CreateApplicationCommandAsync(guildCommand.Build());
+
+                // With global commands we don't need the guild.
+                await client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
+                // Using the ready event is a simple implementation for the sake of the example. Suitable for testing and development.
+                // For a production bot, it is recommended to only run the CreateGlobalApplicationCommandAsync() once for each command.
+            }
+            catch (ApplicationCommandException exception)
+            {
+                // If our command was invalid, we should catch an ApplicationCommandException. This exception contains the path of the error as well as the error message. You can serialize the Error field in the exception to get a visual of where your error is.
+                var json = JsonConvert.SerializeObject(exception.Error, Formatting.Indented);
+
+                // You can send this error somewhere or just print it to the console, for this example we're just going to print it.
+                Console.WriteLine(json);
+            }
         }
     }
 }
