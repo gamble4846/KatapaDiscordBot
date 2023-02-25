6a56ec1412532a7b4657ba3ab5d91bc32d77f2d0
rohanpatel4846 <97083899+rohanpatel4846@users.noreply.github.com>
2023-02-25 16:03:19 +0530

    .

---
+++ KatapaDiscordBot/KatapaDiscordBot/Commands/BasicCommands.cs
@@ -0,0 +1,21 @@
+﻿using Discord.Interactions;
+using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace KatapaDiscordBot.Commands
+{
+    public class BasicCommands
+    {
+        public async Task SlashCommandHandler(SocketSlashCommand command)
+        {
+            await command.RespondAsync($"You executed {command.Data.Name}");
+        }
+    }
+}
--- KatapaDiscordBot/KatapaDiscordBot/Main/Bot.cs
+++ KatapaDiscordBot/KatapaDiscordBot/Main/Bot.cs
@@ -2,11 +2,16 @@
 using Discord;
 using KatapaDiscordBot.Utility;
 using System.Threading.Tasks;
+using Discord.Net;
+using Newtonsoft.Json;
+using System;
+using KatapaDiscordBot.Commands;
 
 namespace KatapaDiscordBot.Main
 {
     public class Bot
     {
+        private DiscordSocketClient _client;
         public async Task RunAsync()
         {
             var ConfigJson = await CommonService.GetConfigJSONObject();
@@ -15,20 +20,20 @@ namespace KatapaDiscordBot.Main
             {
                 GatewayIntents = GatewayIntents.All
             };
+            _client = new DiscordSocketClient(config);
+            _client.Log += Log;
+            var token = ConfigJson.Token;
 
-            DiscordSocketClient client = new DiscordSocketClient(config);
-            await client.LoginAsync(TokenType.Bot, ConfigJson.Token);
-            await client.StartAsync();
-
-            client.MessageReceived += async (message) =>
-            {
-                if (message.Content.StartsWith("!ping"))
-                {
-                    await message.Channel.SendMessageAsync("Pong!");
-                }
-            };
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
 
             await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
